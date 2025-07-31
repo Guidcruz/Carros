43b9751c20ff5e2721b178d0eb0236585d7ba29d
Guilherme Cruz <guidcruz@gmail.com>
2025-07-31 16:24:19 -0300

    Criado Branch app_carro e criação do model Car

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)                     # identificador unico do registro do carro na tabela
+    model = models.CharField(max_length=200)                    # modelo do carro
+    brand = models.CharField(max_length=200)                    # marca
+    factory_year = models.IntegerField(blank=True, null=True)   # ano de fabricacao
+    model_year = models.IntegerField(blank=True, null=True)     # ano do modelo
+    value = models.FloatField(blank=True, null=True)            # valor do carro
